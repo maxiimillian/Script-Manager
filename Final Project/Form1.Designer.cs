b154c2b79c3032d5af57d9303aeee0e446d21c4f
maxiiimillian <maxtbell07@gmail.com>
2021-03-02 02:30:09 -0500

    Adding Files

---
+++ Final Project/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSave = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.styleManager1 = new MetroSet_UI.Components.StyleManager();
+            this.lstBoxProcesses = new System.Windows.Forms.ListBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.inputName = new System.Windows.Forms.TextBox();
+            this.lstBoxTest = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblSave
+            // 
+            this.lblSave.AutoSize = true;
+            this.lblSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(63)))));
+            this.lblSave.Font = new System.Drawing.Font("Bahnschrift Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSave.ForeColor = System.Drawing.Color.White;
+            this.lblSave.Location = new System.Drawing.Point(457, 14);
+            this.lblSave.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSave.MinimumSize = new System.Drawing.Size(200, 0);
+            this.lblSave.Name = "lblSave";
+            this.lblSave.Size = new System.Drawing.Size(200, 23);
+            this.lblSave.TabIndex = 4;
+            this.lblSave.Text = "Create New Save";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(458, 47);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(51, 18);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name:";
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.ForeColor = System.Drawing.Color.White;
+            this.lblFile.Location = new System.Drawing.Point(458, 93);
+            this.lblFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(35, 18);
+            this.lblFile.TabIndex = 7;
+            this.lblFile.Text = "File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(63)))));
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(457, 209);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.MinimumSize = new System.Drawing.Size(351, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(351, 23);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Saved Files";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(63)))));
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Light", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(41, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.MinimumSize = new System.Drawing.Size(351, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 23);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Active Processes";
+            // 
+            // styleManager1
+            // 
+            this.styleManager1.CustomTheme = "C:\\Users\\frogg\\AppData\\Roaming\\Microsoft\\Windows\\Templates\\ThemeFile.xml";
+            this.styleManager1.MetroForm = this;
+            this.styleManager1.Style = MetroSet_UI.Enums.Style.Custom;
+            this.styleManager1.ThemeAuthor = "Narwin";
+            this.styleManager1.ThemeName = "MetroDark";
+            // 
+            // lstBoxProcesses
+            // 
+            this.lstBoxProcesses.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(47)))), ((int)(((byte)(51)))));
+            this.lstBoxProcesses.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxProcesses.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lstBoxProcesses.ForeColor = System.Drawing.Color.White;
+            this.lstBoxProcesses.FormattingEnabled = true;
+            this.lstBoxProcesses.ItemHeight = 18;
+            this.lstBoxProcesses.Location = new System.Drawing.Point(44, 70);
+            this.lstBoxProcesses.Name = "lstBoxProcesses";
+            this.lstBoxProcesses.Size = new System.Drawing.Size(348, 432);
+            this.lstBoxProcesses.TabIndex = 21;
+            this.lstBoxProcesses.DoubleClick += new System.EventHandler(this.lstBoxProcesses_DoubleClick);
+            // 
+            // btnFile
+            // 
+            this.btnFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.btnFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFile.FlatAppearance.BorderSize = 0;
+            this.btnFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFile.ForeColor = System.Drawing.Color.White;
+            this.btnFile.Location = new System.Drawing.Point(527, 87);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(129, 30);
+            this.btnFile.TabIndex = 23;
+            this.btnFile.Text = "Browse";
+            this.btnFile.UseVisualStyleBackColor = false;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(184)))), ((int)(((byte)(196)))));
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Bahnschrift SemiBold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.Black;
+            this.btnSave.Location = new System.Drawing.Point(459, 139);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(197, 36);
+            this.btnSave.TabIndex = 24;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputName
+            // 
+            this.inputName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.inputName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputName.ForeColor = System.Drawing.Color.White;
+            this.inputName.Location = new System.Drawing.Point(527, 45);
+            this.inputName.MinimumSize = new System.Drawing.Size(2, 30);
+            this.inputName.Name = "inputName";
+            this.inputName.Size = new System.Drawing.Size(129, 26);
+            this.inputName.TabIndex = 25;
+            // 
+            // lstBoxTest
+            // 
+            this.lstBoxTest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(47)))), ((int)(((byte)(51)))));
+            this.lstBoxTest.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstBoxTest.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lstBoxTest.ForeColor = System.Drawing.Color.White;
+            this.lstBoxTest.FormattingEnabled = true;
+            this.lstBoxTest.ItemHeight = 18;
+            this.lstBoxTest.Location = new System.Drawing.Point(459, 265);
+            this.lstBoxTest.Name = "lstBoxTest";
+            this.lstBoxTest.Size = new System.Drawing.Size(349, 234);
+            this.lstBoxTest.TabIndex = 26;
+            this.lstBoxTest.DoubleClick += new System.EventHandler(this.lstBoxTest_DoubleClick);
+            this.lstBoxTest.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lstBoxTest_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.ClientSize = new System.Drawing.Size(884, 623);
+            this.Controls.Add(this.lstBoxTest);
+            this.Controls.Add(this.inputName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.lstBoxProcesses);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblSave);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSave;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private MetroSet_UI.Components.StyleManager styleManager1;
+        private System.Windows.Forms.TextBox inputName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.ListBox lstBoxProcesses;
+        private System.Windows.Forms.ListBox lstBoxTest;
+    }
+}
+
---
+++ Final Project/Form1.cs
@@ -0,0 +1,289 @@
+﻿/* By: Sinew
+ * Date: 03/01/2021
+ * Desc: Save, run, and manage scripts or files in one location.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Final_Project
+{
+
+    public partial class Form1 : Form
+    {
+        public static string path = "";
+        public static string dbPath = "Data Source=C:/Users/frogg/source/repos/Final Project/Final Project/db.db";
+        public static IDictionary<string, System.Diagnostics.Process> processes = new Dictionary<string, System.Diagnostics.Process>();
+        public static Button deleteButton = new Button();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        //Async function allows program to wait for a program opened through it to be closed.
+        private async Task waitForProcessAsync(Process p)
+        {
+            if (p == null) return;
+            try
+            {
+                while (!p.HasExited)
+                    await Task.Delay(100);
+            }
+            catch (NullReferenceException)
+            {
+                return;
+            }
+        }
+
+        //Returns the file path of an item using its name
+        private string getPath(string name)
+        {
+            using (var connection = new SqliteConnection(dbPath))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = @"SELECT path FROM scripts WHERE name=$name";
+                command.Parameters.AddWithValue("$name", name);
+
+                var value = command.ExecuteReader();
+                while (value.Read())
+                {
+                    return Convert.ToString(value["path"]);
+                }
+                return "err";
+            }
+        }
+
+        //Loads all saved scripts on open
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var connection = new SqliteConnection(dbPath))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = @"SELECT * FROM scripts";
+
+                var value = command.ExecuteReader();
+                while (value.Read())
+                {
+                    lstBoxTest.Items.Add(Convert.ToString(value["name"]));
+
+                }
+
+            }
+        }
+
+        private void lstBoxTest_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Check if click is left or right and its locaiton
+            Point p = new Point(e.X, e.Y);
+            object item;
+
+            //Stops the program from clicking where theres no box
+            try
+            {
+                
+                item = lstBoxTest.Items[lstBoxTest.IndexFromPoint(p)];
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                //Only one delete button at a time
+                if (deleteButton != null) this.Controls.Remove(deleteButton); ;
+
+                var deleteBtn = new Button();
+                deleteBtn.Name = "deleteBtn";
+                deleteBtn.Text = "Delete";
+                deleteBtn.BackColor = Color.FromArgb(199, 66, 66);
+                deleteBtn.FlatStyle = FlatStyle.Flat;
+                deleteBtn.ForeColor = Color.White;
+                deleteBtn.Size = new Size(75, 25);
+
+                deleteBtn.Cursor = Cursors.Hand;
+
+                deleteBtn.Click += (s, ev) => 
+                {
+                    using (var connection = new SqliteConnection(dbPath))
+                    {
+                        connection.Open();
+
+                        var command = connection.CreateCommand();
+                        command.CommandText = @"DELETE FROM scripts WHERE name=$name";
+                        command.Parameters.AddWithValue("$name", item.ToString());
+
+                        command.ExecuteNonQuery();
+
+                    }
+                    lstBoxTest.Items.Remove(item);
+                    this.Controls.Remove(deleteBtn);
+                };
+
+                deleteBtn.Location = this.PointToClient(Cursor.Position);
+                Controls.Add(deleteBtn);
+                deleteBtn.BringToFront();
+
+                deleteButton = deleteBtn;
+            }
+        }
+
+        private void form1_Click(object sender, EventArgs e)
+        {
+            //Remove the delete button if client clicks anywhere off of it
+            try
+            {
+                this.Controls.Remove(deleteButton);
+                deleteButton = null;
+
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+       
+
+        private async void lstBoxTest_DoubleClick(object sender, EventArgs e)
+        {
+            Process p;
+            try
+            {
+                /*Might be able to do this with location instead of looping? 
+                 * Currently checks for the path of the active process clicked and 
+                 * starts that program
+                 */
+                foreach (var item in lstBoxTest.SelectedItems)
+                {
+                    try
+                    {
+                        string filePath = getPath(item.ToString());
+
+                        lstBoxProcesses.Items.Add(item.ToString());
+
+                        p = Process.Start(filePath);
+                        processes.Add(item.ToString(), p);
+                    }
+                    catch (Exception)
+                    {
+                        return;
+                    }
+                    //This is to eat up any errors raised by the opened programming not persisting
+                    // I.e a jpg 
+                    try
+                    {
+                        await waitForProcessAsync(p);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                    processes.Remove(item.ToString());
+                    for (int n = lstBoxProcesses.Items.Count - 1; n >= 0; --n)
+                    {
+                        if (lstBoxProcesses.Items[n].ToString().Contains(item.ToString()))
+                        {
+                            lstBoxProcesses.Items.RemoveAt(n);
+                        }
+                    }
+
+
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+
+        }
+
+        private void lstBoxProcesses_DoubleClick(object sender, EventArgs e)
+        {
+            //Once again may be able to do this with location later
+            //Brings clicked process into focus
+            foreach (var item in lstBoxProcesses.SelectedItems)
+            {
+                SetForegroundWindow(processes[item.ToString()].MainWindowHandle);
+                return;
+            }
+        }
+
+        private void textBoxProcesses_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        //Dialog to upload the file to save
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                btnFile.Text = fd.SafeFileName;
+                path = fd.FileName;
+            }
+        }
+
+        //Saving the name and file path into sqlite
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = inputName.Text;
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(path))
+            {
+                lblSave.Text = "You can't leave any boxes empty";
+                return;
+            }
+
+            using (var connection = new SqliteConnection(dbPath))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                    @"INSERT INTO scripts (name, path) VALUES ($name,$path)";
+                command.Parameters.AddWithValue("$name", name);
+                command.Parameters.AddWithValue("$path", path);
+
+                command.ExecuteNonQuery();
+
+            }
+            lstBoxTest.Items.Add(name);
+            inputName.Text = "";
+            btnFile.Text = "Browse";
+            path = "";
+            lblSave.Text = "Create New Save";
+        }
+
+        private void lstBoxProcesses_SelectedIndexChanged(object sender)
+        {
+
+        }
+
+        private void inputName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
